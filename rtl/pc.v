a9852f6dc5fa2743a490080dc4794d93d7dc8069
sanika-beep <sanikampatil04@gmail.com>
2026-06-08 16:37:28 +0530

    Add Program Counter module

---
+++ rtl/pc.v
@@ -0,0 +1,16 @@
+module pc(
+    input clk,
+    input rst,
+    input [31:0] next_pc,
+    output reg [31:0] pc
+);
+
+always @(posedge clk or posedge rst)
+begin
+    if(rst)
+        pc <= 32'd0;
+    else
+        pc <= next_pc;
+end
+
+endmodule
